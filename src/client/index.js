004849991320613c42f2900ae9ddf6ca1f8332c8
Joey Berger <44282450+joeyBerger@users.noreply.github.com>
2021-09-05 15:19:30 -0700

    implemented scss files, reorg of index.html and index.js

--- src/client/index.js
+++ src/client/index.js
@@ -4,19 +4,21 @@ import {handleAPIrequest} from './js/handleAPIrequest'
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/form.scss'
+import './styles/section.scss'
 import './styles/footer.scss'
 import './styles/header.scss'
 
-const results = document.getElementById('results');
+import './styles/custom-styling.scss'
+
+const results = document.getElementById('results-master-container');
 results.style.display = 'none';
 
-const submittedURL = document.getElementById('submitted-url');
 const testButton = document.getElementById('submit-button');
 
 const errorMessage = document.getElementById('error-message');
 errorMessage.style.display = 'none';
 
-const resultsContainer = document.getElementById('results-container');
+const resultsContainer = document.getElementById('results-data-container');
 
 testButton.addEventListener('click', async (e) => {
     let url;
@@ -24,24 +26,31 @@ testButton.addEventListener('click', async (e) => {
     errorMessage.style.display = 'none'
     try {url = handleSubmit(e)}
     catch (e) {
-        errorMessage.innerHTML = e;
+        errorMessage.innerHTML = `Error : ${e}`;
         errorMessage.style.display = 'block'
         return
     }
     const res = await handleAPIrequest(url)
     results.style.display = 'block';
-    submittedURL.innerHTML = url;
 
     //remove any existing children from results container
     while (resultsContainer.firstChild) {
         resultsContainer.removeChild(resultsContainer.firstChild);
     }
 
+    //add submitted URL
+    const enteredURL = document.createElement("div");
+    enteredURL.innerHTML = `URL : ${url}`;
+    enteredURL.classList.add('data-piece');
+    resultsContainer.appendChild(enteredURL);
+
+
     //display results for each data piece
     const subjects = ["confidence","subjectivity","score","agreement","irony"]
     subjects.forEach(k => {
-        const newDiv = document.createElement("div");
-        newDiv.innerHTML = `${k.slice(0,1).toUpperCase()}${k.slice(1)} : ${res[k]}`;
-        resultsContainer.appendChild(newDiv);
+        const dataDiv = document.createElement("div");
+        dataDiv.innerHTML = `${k.slice(0,1).toUpperCase()}${k.slice(1)} : ${res[k]}`;
+        dataDiv.classList.add('data-piece');
+        resultsContainer.appendChild(dataDiv);
     })
 })

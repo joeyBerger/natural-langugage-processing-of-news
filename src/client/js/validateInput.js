aedf1dfd8e6b247226aba637c555e910b947d572
Joey Berger <44282450+joeyBerger@users.noreply.github.com>
2021-09-05 10:32:09 -0700

    Update validateInput.js

--- src/client/js/validateInput.js
+++ src/client/js/validateInput.js
@@ -1,6 +1,7 @@
 import {errorCodes} from "./errorCodes";
 
 const returnIsValidURL = (url) => {
+    //https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
     var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
         '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
         '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
